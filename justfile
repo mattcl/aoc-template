745877007164a03155bcf098fedab059584391e6
Matt Chun-Lum <matt@questionable.engineering>
2022-12-24 23:46:06 -0800

    chore: switch to aoc-std, enable more crates by default

--- justfile
+++ justfile
@@ -2,6 +2,10 @@
 new DAY NAME:
     scripts/new.sh {{DAY}} {{NAME}}
 
+# run all integration tests
+test:
+    cargo test --release -- --ignored
+
 # run benchmarks for a given padded day `just bench 001`
 bench DAY:
     cargo bench -p aoc-benchmarking -- {{DAY}}
@@ -10,5 +14,10 @@ bench DAY:
 bench_all:
     cargo bench -p aoc-benchmarking
 
+# makes a flamegraph for the given day
 flame DAY:
     scripts/flame.sh {{DAY}}
+
+# builds the cli
+build-cli:
+    cargo build -p aoc-cli --release
--- template/src/lib.rs
+++ template/src/lib.rs
@@ -2,7 +2,7 @@ use std::str::FromStr;
 
 use aoc_plumbing::Problem;
 
-#[derive(Debug, Clone, Copy, Eq, PartialEq)]
+#[derive(Debug, Clone)]
 pub struct {{project-name|upper_camel_case}};
 
 impl FromStr for {{project-name|upper_camel_case}} {
@@ -19,15 +19,15 @@ impl Problem for {{project-name|upper_camel_case}} {
     const README: &'static str = include_str!("../README.md");
 
     type ProblemError = anyhow::Error;
-    type P1 = &'static str;
-    type P2 = &'static str;
+    type P1 = &'static i64;
+    type P2 = &'static i64;
 
     fn part_one(&mut self) -> Result<Self::P1, Self::ProblemError> {
-        Ok("not implemented")
+        Ok(0)
     }
 
     fn part_two(&mut self) -> Result<Self::P2, Self::ProblemError> {
-        Ok("not implemented")
+        Ok(0)
     }
 }
 
@@ -42,13 +42,13 @@ mod tests {
     fn full_dataset() {
         let input = std::fs::read_to_string("input.txt").expect("Unable to load input");
         let solution = {{project-name|upper_camel_case}}::solve(&input).unwrap();
-        assert_eq!(solution, Solution::new("not implemented", "not implemented"));
+        assert_eq!(solution, Solution::new(0, 0));
     }
 
     #[test]
     fn example() {
         let input = "";
         let solution = {{project-name|upper_camel_case}}::solve(input).unwrap();
-        assert_eq!(solution, Solution::new("not implemented", "not implemented"));
+        assert_eq!(solution, Solution::new(0, 0));
     }
 }
